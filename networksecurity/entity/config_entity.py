b44e17b411fff6b4e441882156abd8f671ecb28c
NikhilYadavCoder <nikhilgolla9@gmail.com>
2025-08-09 22:25:53 +0530

    trainer evaluation and mlflow completed

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -3,6 +3,7 @@ import os
 from networksecurity.constant import training_pipeline
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.constant.training_pipeline import MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD
 
 print(training_pipeline.PIPELINE_NAME)
 print(training_pipeline.ARTIFACT_DIR)
@@ -58,3 +59,18 @@ class DataTransformationConfig:
             training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
         self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
             training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
+
+
+
+class ModelTrainerConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_trainer_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.MODEL_TRAINER_DIR_NAME
+        )
+        self.trained_model_file_path: str = os.path.join(
+            self.model_trainer_dir, training_pipeline.MODEL_TRAINER_TRAINED_MODEL_DIR, 
+            training_pipeline.MODEL_FILE_NAME
+        )
+        self.expected_accuracy: float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE
+        self.overfitting_underfitting_threshold = training_pipeline.MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD
+        
